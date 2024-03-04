501ee85eabe676c4474875020327e1f740eb0cf2
olli <olli@>
2024-03-04 11:42:56 +0200

    Online deploy

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
-
+app.use(morgan('tiny')) 
+app.use(cors())
 
 
 let persons = [
@@ -90,7 +93,7 @@ app.get('/', (request, response) => {
     response.json(newPerson)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
